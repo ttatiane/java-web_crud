625c24fc80d08954eedd05a7523c4d054366bca8
Tatiane Takeuti <tatiane.takeuti@hotmail.com>
2020-06-14 00:11:38 -0300

    Complementando metadados nas Entidades.

--- main/java/com/cadastro_empresarial/erp/model/RamoAtividade.java
+++ main/java/com/cadastro_empresarial/erp/model/RamoAtividade.java
@@ -2,6 +2,7 @@ package com.cadastro_empresarial.erp.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class RamoAtividade implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(nullable = false, length = 80)
 	private String descricao;
 
 	public Long getId() {
